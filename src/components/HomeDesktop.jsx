77ab689131d527c410edfe3a52eab9fce82934a4
BRUNO EMANUEL TRAJANO <05260437098@senacrs.edu.br>
2025-09-11 10:34:11 -0300

    adiciona header na home page para telas lg+

---
+++ src/components/HomeDesktop.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import Logo from "../../public/logo-pacenatela.svg";
+
+
+export default function HomeDesktop() {
+    return (
+        <div className="bg-[#D8DFE3] h-screen w-screen flex-col justify-center">
+            <header className="border-b-2 flex border-[#C7CBCE] w-[90vw] h-20 justify-around">
+            <Image src={Logo} alt="logo pace na tela" className="w-20 h-20" />
+            
+            <ul className="flex justify-center items-center ml-20 gap-10 font-light">
+                <li>Diferenciais</li>
+                <li>Integrações</li>
+                <li>Rodapé</li>
+                <li>Sobre</li>
+                <li>Entrar</li>
+            </ul>
+
+            <div className="flex items-center">
+                <button className="rounded-2xl border border-blue-600 text-blue-600 w-48 h-10 flex items-center justify-center">Carregar atividade</button>
+            </div>
+
+            </header>
+
+            <div className="hero flex text-5xl font-extrabold text-center justify-center items-center">
+                <h1 className="mt-10">UMA NOVA FORMA<br/><h1 className="mt-5 border-2 border-[#C7CBCE] rounded-full p-5">DE COMPARTILHAR</h1><br/>SUAS CORRIDAS</h1>
+            </div>
+        </div>
+    )
+}
